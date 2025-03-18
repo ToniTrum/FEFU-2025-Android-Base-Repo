efa35f56bae0e110084e64db66b5ca5c5140383c
ToniTrum <kulahszjan@gmail.com>
2025-03-18 19:18:41 +1000

    finish task 3

--- app/src/main/java/co/feip/fefu2025/MainActivity.kt
+++ app/src/main/java/co/feip/fefu2025/MainActivity.kt
@@ -1,9 +1,13 @@
 package co.feip.fefu2025
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import co.feip.fefu2025.ui.theme.languageMap
+import views.CustomLayout
 import views.LanguageInformationView
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +15,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val languageInformationView: LanguageInformationView = findViewById(R.id.viewLI)
-        languageInformationView.setText("Kotlin", 100f)
-        languageInformationView.setDotColor(0xFFFF0000.toInt())
+        val customLayout: CustomLayout = findViewById(R.id.vgCustomLayout)
+        val btnAdd: Button = findViewById(R.id.btnAdd)
+
+        btnAdd.setOnClickListener {
+            val randomLanguage: String = languageMap.keys.random()
+            val randomPercent: Float = ((Random.nextDouble(0.0, 100.0) * 10).toInt() / 10.0).toFloat()
+
+            val newLanguageInformationView = LanguageInformationView(this)
+            newLanguageInformationView.setText(randomLanguage, randomPercent)
+            newLanguageInformationView.setDotColor(languageMap[randomLanguage] ?: 0xFF000000.toInt())
+
+            customLayout.addView(newLanguageInformationView)
+            customLayout.requestLayout()
+        }
     }
 }
